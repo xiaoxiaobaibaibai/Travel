c46370176eac55b80a69508af8cd6510dd7de83e
bai <2516220191@qq.com>
2020-02-04 14:23:56 +0800

    Redundant code finishing 20200204

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import './assets/styles/reset.css'
+// 重置不同端的样式
+import './assets/styles/border.css'
+// 移动端1像素边框问题
 
 Vue.config.productionTip = false
 
