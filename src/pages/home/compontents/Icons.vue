bbf37ecfb5dbca74e1cef0d48fb205a02c49a769
bai <2516220191@qq.com>
2020-02-09 19:35:22 +0800

    index finish

--- src/pages/home/compontents/Icons.vue
+++ src/pages/home/compontents/Icons.vue
@@ -1,6 +1,6 @@
 <template>
     <div class= "icons">
-      <swiper>
+      <swiper :options="swiperOption">
         <swiper-slide v-for="(page,index) of pages" :key="index">
         <div
           class="icon"
@@ -21,6 +21,9 @@ export default {
   name: 'HomeIcons',
   data () {
     return {
+      swiperOption: {
+        autoplay: false
+      },
       iconList: [{
         id: '0001',
         imgUrl: require('./img/XEM.png'),
